73961fad129baac711c203f92bafc5ae981e4726
samzhuwj <samzhuwj@gmail.com>
2017-11-17 23:43:36 +0800

    db add status to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171116010933) do
+ActiveRecord::Schema.define(version: 20171117154059) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20171116010933) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "friendly_id"
+    t.string "status", default: "draft"
     t.index ["friendly_id"], name: "index_events_on_friendly_id", unique: true
   end
 
